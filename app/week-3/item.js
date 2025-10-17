42caef939b070a116740dfc5dc4fae16009c8261
Devp139 <devkumarmanojbhai.chhatrala@edu.sait.ca>
2025-10-17 16:58:20 -0600

    Updated week-6 code

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,13 +1,10 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="bg-slate-800 text-white p-4 rounded-md shadow-md">
-      <div className="font-bold text-lg">{name}</div>
-      <div className="text-gray-300">
+    <li className="p-2 mb-2  bg-gray-800 rounded w-65 mx-auto">
+      <div className="font-semibold text-lg">{name}</div>
+      <div className="text-sm text-white-900-bold">
         Buy {quantity} in {category}
       </div>
     </li>
   );
 }
-
-
-
